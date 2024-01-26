11de803bdf602b72fccccc2d20cd9696cfc7d9f7
Anchit <anchitnayak2003@gmail.com>
2024-01-26 18:40:46 +0530

    Signup and Signin page added

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
+import signupImg from '../assets/images/signup.jpg'
+import { Link, useNavigate } from 'react-router-dom'
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
+    return <section className="px-5 lg:px-0">
+    <div className="w-full max-w-[570px] mx-auto rounded-lg shadow-md md:p-10">
+      <h3 className='text-headingColor text-[22px] leading-9 font-bold mb-10'>Hello <span className='text-primaryColor'>Welcome</span> Back 🙋</h3>
+      <form className='py-4 md:py-0' onSubmit={''}>
+        <div className="mb-5">
+          <input type="email" placeholder='Enter Your Email' name='email' className='w-full py-3 border-b bprder-solid border-[#0066ff61] focus:outline-none focus:border-b-primaryColor text-[16px] leading-7 text-headingColor placeholder:text-textColor cursor-pointer' required />
+        </div>
+
+        <div className="mb-5">
+          <input type="password" placeholder='Password' name='password' className='w-full py-3 border-b bprder-solid border-[#0066ff61] focus:outline-none focus:border-b-primaryColor text-[16px] leading-7 text-headingColor placeholder:text-textColor cursor-pointer' required />
+        </div>
+
+        <div className="mt-7">
+          <button type='submit' className="w-full bg-primaryColor text-white text-[18px] leading-[30px] rounded-lg px-4 py-3 flex items-center justify-center">
+            Login
+            {/* <Loader onClick={handleButtonClick}isLoading={isLoading} buttonText="Log in"/> */}
+            </button>
+        </div>
+
+        <p className="mt-5 text-textColor text-center">Don't have an account ? <Link to='/signup' className='text-primaryColor font-medium ml-1'>Register</Link></p>
+
+      </form>
+    </div>
+  </section>
 }
 
 export default Login
